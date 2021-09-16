de0500a8670d789d70c4715f0967871381eef044
mglight <88080931+mglight@users.noreply.github.com>
2021-09-16 22:48:06 +0800

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,30 @@
+let heading = document.querySelector('h1');
+
+function identity() {
+  let Name = prompt('请输入身份证：');
+  localStorage.setItem('name', Name);
+  password();
+}
+
+function password() {
+  let Passworda = prompt('请输入密码：');
+  localStorage.setItem('password', Passworda);
+  again();
+}
+
+function again() {
+  let Passwordb = prompt('确认您的密码：');
+  if(localStorage.getItem('password')===Passwordb) {
+    let User = localStorage.getItem('name');
+    heading.textContent = '欢迎光临' + User;
+  } else {
+    alert('密码不正确！');
+    again();
+  }
+}
+
+if(localStorage.getItem('name')) {
+  again();
+} else {
+  identity();
+} 

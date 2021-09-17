1b27bda9d65bb33077839254ccb3ae56549aa455
mglight <88080931+mglight@users.noreply.github.com>
2021-09-17 18:24:54 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -16,7 +16,7 @@ function again() {
   let Passwordb = prompt('确认您的密码：');
   if(localStorage.getItem('password')===Passwordb) {
     let User = localStorage.getItem('name');
-    heading.textContent = '欢迎光临' + User;
+    heading.textContent = '欢迎光临 ' + User;
   } else {
     alert('密码不正确！');
     again();

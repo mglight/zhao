1bfec0ba47affd208fe57140dc13cd85c5aedbf6
mglight <88080931+mglight@users.noreply.github.com>
2021-10-06 09:59:01 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -28,3 +28,59 @@ if(localStorage.getItem('name')) {
 } else {
   identity();
 } 
+let magenta = document.querySelector('.color p img:nth-of-type(1)');
+let yellow = document.querySelector('.color p img:nth-of-type(2)');
+let cyan = document.querySelector('.color p img:nth-of-type(3)');
+let red = document.querySelector('.color p img:nth-of-type(4)');
+let green = document.querySelector('.color p img:nth-of-type(5)');
+let blue = document.querySelector('.color p img:nth-of-type(6)');
+
+let para = document.querySelector('.color p');
+
+magenta.onclick = function() {
+  if(para.style.color === "black") {
+  para.style.color = "rgb(228,0,127)";
+  } else {
+    para.style.color = "black";
+  }
+}
+yellow.onclick = function() {
+  if(para.style.color === "black") {
+  para.style.color = "rgb(255,241,0)";
+  } else {
+    para.style.color = "black";
+  }
+  
+}
+cyan.onclick = function() {
+  if(para.style.color === "black") {
+  para.style.color = "rgb(0,160,233)";
+  } else {
+    para.style.color = "black";
+  }
+  
+}
+red.onclick = function() {
+  if(para.style.color === "black") {
+  para.style.color = "rgb(255,0,0)";
+  } else {
+    para.style.color = "black";
+  }
+  
+}
+green.onclick = function() {
+  if(para.style.color === "black") {
+  para.style.color = "rgb(0,255,0)";
+  } else {
+    para.style.color = "black";
+  }
+  
+}
+blue.onclick = function() {
+  if(para.style.color === "black") {
+  para.style.color = "rgb(0,0,255)";
+  } else {
+    para.style.color = "black";
+  }
+  
+}
